4b7e1eaf99b9b5d07bc4602775728ffda3bd2632
abedinalways <nion.sk5067@gmail.com>
2025-08-13 01:35:50 +0600

    input output

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Enter values for X and Y: ";
+    double X;
+    double Y;
+    cin>>X>>Y;
+    // cin>>Y;
+   cout<<X+Y<<endl;
+    cout<<"Total-Value: "<<X+Y<<endl;
+    return 0;
+}
